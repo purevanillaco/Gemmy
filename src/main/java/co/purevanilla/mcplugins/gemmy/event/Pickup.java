4e78cbefc65aeeb0c40bafbfd3f27631f38f2255
quiquelhappy <quiquelhappy@gmail.com>
2023-09-01 12:06:54 +0200

    add missing handler method

--- src/main/java/co/purevanilla/mcplugins/gemmy/event/Pickup.java
+++ src/main/java/co/purevanilla/mcplugins/gemmy/event/Pickup.java
@@ -3,13 +3,18 @@ package co.purevanilla.mcplugins.gemmy.event;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
 public class Pickup extends Event {
 
     private static final HandlerList HANDLERS_LIST = new HandlerList();
 
     @Override
-    public HandlerList getHandlers() {
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
         return HANDLERS_LIST;
     }
 
